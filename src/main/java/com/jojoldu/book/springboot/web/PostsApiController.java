68b226b3d90fa51b588e9be683c44134940eedbd
njg7194 <njg7194@naver.com>
2021-06-02 10:59:02 +0900

    21.6.1 공익(중) 커밋/p.118 : H2 인메모리 데이터베이스 테스트 쿼리 실행 성공.

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -24,7 +24,7 @@ public class PostsApiController {
         return postsService.update(id, requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id]}")
+    @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById (@PathVariable Long id) {
         return postsService.findById(id);
     }
