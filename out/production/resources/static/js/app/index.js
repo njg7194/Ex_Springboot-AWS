61da0fb5ea1e995d9faaf56ba864726c614c1f07
njg7194 <njg7194@naver.com>
2021-06-08 09:53:39 +0900

    21.6.8 / 공익 중 커밋 / p.142 : 글 입력 버튼 작동 아직 못고침. 집에서 PR후 merge후 새 commit.

---
+++ out/production/resources/static/js/app/index.js
@@ -0,0 +1,32 @@
+var main = {
+    init : function(){
+        console.log("hello");
+        var _this = this;
+        $('#btn-save').on('click', function () {
+            console.log("Clicked");
+            _this.save();
+            
+        });
+    },
+    save : function () {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+
+main.init();
