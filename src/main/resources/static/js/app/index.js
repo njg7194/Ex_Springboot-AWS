fb64632e322497c5e4642689ab255899d6460283
njg7194 <njg7194@naver.com>
2021-06-09 18:01:12 +0900

    21.6.9 / 공익 퇴근 커밋 / p.154 : 게시글 수정/삭제 화면 작성중...

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,13 +1,14 @@
 var main = {
 
-    init : function(){
-        console.log("hello");
+    init : function() {
         var _this = this;
         $('#btn-save').on('click', function () {
-            console.log("Clicked");
             _this.save();
-            
         });
+
+        $('#btn-update').on('click'), function () {
+            _this.update();
+        }
     },
     save : function () {
         var data = {
@@ -27,6 +28,29 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
+    },
+
+    update : function () {
+        var data = {
+            title: $('#title').val,
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+        
     }
 };
 
