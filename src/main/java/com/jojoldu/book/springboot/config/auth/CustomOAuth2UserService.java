f6a42dcbd998c140cde64f79e60739c542fdf142
njg7194 <njg7194@naver.com>
2021-06-24 15:30:28 +0900

    공익 중 커밋 / p.210 : 네이버 로그인 동의 화면 접근 완료. 동의하기 누를 시 토큰 전달 오류로 동작하지 않음.

--- src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -50,9 +50,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         return new DefaultOAuth2User(
                 Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
                 attributes.getAttributes(),
-                attributes.getNameAttrubuteKey());
-
-
+                attributes.getNameAttributeKey());
     }
 
     private User saveOrUpdate(OAuthAttributes attributes) {
