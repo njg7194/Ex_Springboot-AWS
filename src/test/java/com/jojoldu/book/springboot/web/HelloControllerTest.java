c7c1fe2f9afd75bad7d35c2b68a189cd6e44763d
no jae gun <njg7194@naver.com>
2021-07-08 17:25:58 +0900

    마스터에 제대로 적용 안되서 적용하기 위한 커밋

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+//
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class, excludeFilters = {
