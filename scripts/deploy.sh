3ee00322383f064db23ba7dca482ca0a8b40eaae
njg7194 <njg7194@naver.com>
2021-07-14 06:53:34 +0900

    배포파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ CURRENT_PID=$(pgrep -fl Ex_Springboot-AWS | grep jar | awk 'print $1')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID"]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
@@ -34,5 +34,5 @@ echo "> $JAR_NAME 실행"
 
 nohup java -jar \
 -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
--Dspring.profiles.active=real\
+-Dspring.profiles.active=real \
 $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
