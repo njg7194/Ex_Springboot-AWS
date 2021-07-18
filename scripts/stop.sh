40c94774c23ac058d442d27940f66af77ac44b8d
no jae gun <njg7194@naver.com>
2021-07-18 17:02:16 +0900

    집에서 커밋 / p.395 : 무중단 배포 스크립트 만들기. nginx 실행 관련 스크립트 수정.

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -7,7 +7,7 @@ source ${ABSDIR}/profile.sh
 IDLE_PORT=$(find_idle_port)
 
 echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
-IDLE_PID=$(lsof -ti tcp:${IDLE_PORT}) #IDLE_PORT에서 PID로 수정
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
