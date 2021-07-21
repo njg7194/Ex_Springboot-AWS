c66ff6905432bcaab7a5c12b21b623dd00025b88
njg7194 <njg7194@naver.com>
2021-07-21 17:07:31 +0900

    집 커밋 / p.395 : 무중단 배포 스크립트 만들기. nginx 실행 관련 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션pid 확인"
 
-CURRENT_PID=$(pgrep -fl Ex_Springboot-AWS | grep jar | awk '{print $1}')
+CURRENT_PID=$(ps -ef | grep ${PROJECT_NAME} | grep jar | awk '{print $2}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
